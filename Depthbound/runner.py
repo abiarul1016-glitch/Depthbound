ba5780aaadc6b96609713286f260794f4579cd45
Abishan <abiarul1016@gmail.com>
2026-02-28 14:39:28 -0500

    Add files via upload

---
+++ Depthbound/runner.py
@@ -0,0 +1,16 @@
+def main():
+    # Display story of main character having argument and being pushed down into the sea (to the bottom,
+    # possibly show visuals of the ice and lava in this world)
+
+    # Display visual of some god (or something) giving the player a water mask,
+    # and start the game in the ocean
+
+    # Eventually switch between lava arena and ocean
+    # (while occasionaly bringing in the ice landscape)
+
+    # When player loses, offer them a respawn, if they wish to vote for Abishan
+    # (this feature is only enabled in the demo version, not the public version)
+
+
+if __name__ == '__main__':
+    main()
